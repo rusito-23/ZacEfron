d4d25e0f571f0e6e95f75b6540f735105c824418
Igor Andruskiewitsch <igor.andruskiewitsch@incluit.com>
2019-06-21 14:47:06 -0300

    Separate crontab creation for mac and linux

--- zac-efron.sh
+++ zac-efron.sh
@@ -22,7 +22,41 @@ cp jobs/$OS.sh $ROOT_FOLDER/.job.sh
 chmod +x $ROOT_FOLDER/.randomizer.sh
 chmod +x $ROOT_FOLDER/.job.sh
 
+
 # setup cronjob
-ssh localhost "(crontab -l -u $USER 2>/dev/null; \
-                echo '* * * * * ${ROOT_FOLDER}/.job.sh ${ROOT_FOLDER} > ${ROOT_FOLDER}/.logs 2>&1') \
-                | crontab -"
+if [[ "$OS" == "MAC" ]]; then # MAC CASE
+
+    PLIST_NAME=com.zac.efron.plist
+    echo '
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>com.zac.efron</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>${ROOT_FOLDER}/.job.sh</string>
+      </array>
+      <key>Nice</key>
+      <integer>1</integer>
+      <key>StartInterval</key>
+      <integer>60</integer>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>StandardErrorPath</key>
+      <string>${ROOT_FOLDER}/.logs</string>
+      <key>StandardOutPath</key>
+      <string>${ROOT_FOLDER}/.logs</string>
+    </dict>
+    </plist>
+    ' > $HOME/Library/LaunchAgents/$PLIST_NAME
+    launchctl load com.alvin.crontabtest.plist
+
+else # LINUX CASE
+
+    (crontab -l -u $USER 2>/dev/null; \
+    echo '* * * * * ${ROOT_FOLDER}/.job.sh ${ROOT_FOLDER} > ${ROOT_FOLDER}/.logs 2>&1') \
+    | crontab -
+
+fi
